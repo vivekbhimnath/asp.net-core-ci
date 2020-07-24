9ae05f37f50e80f6d94e400b7f3d056a34024aef
Mahendra Shinde <MahendraShinde@synergetics-india.com>
2020-07-24 12:49:24 +0530

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace asp.net_core_ci.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
